7033d9b6a7de12db453cd87df1c5007e139ae69c
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-07-06 11:14:58 +0700

    update

---
+++ readme.md
@@ -0,0 +1,9 @@
+# FO3 ECC Project
+
+## Setup
+
+*  Create new database.
+
+*  Run `sh setup.sh` in root directory.
+
+Please remember you must have redis server in your localhost.
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+echo "Start setup..."
+
+read -p "Your MySQL username : "  mysql_username
+read -p "Your MySQL password : "  mysql_password
+read -p "Your MySQL database : "  mysql_database_name
+
+cp .env.testing .env
+sed -i -e "s/DB_DATABASE=homestead/DB_DATABASE=$mysql_database_name/g" .env
+sed -i -e "s/DB_USERNAME=homestead/DB_USERNAME=$mysql_username/g" .env
+sed -i -e "s/DB_PASSWORD=secret/DB_PASSWORD=$mysql_password/g" .env
+sed -i -e "s/SESSION_DRIVER=file/SESSION_DRIVER=redis/g" .env
+
+touch storage/logs/laravel.log
+chmod -R 777 storage
+composer install
+php artisan migrate
+php artisan db:seed
+php artisan key:generate
+
+echo "Done."
